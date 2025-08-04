a78a699387c49a1a4204197a2a9d7051d55fc0ed
marinemood-web <marinemood@gmail.com>
2025-08-04 19:20:55 +0200

    Update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -1,5 +1,6 @@
 const { FusesPlugin } = require('@electron-forge/plugin-fuses');
 const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const AutoUnpackNativesPlugin = require('@electron-forge/plugin-auto-unpack-natives');
 
 module.exports = {
   packagerConfig: {
@@ -16,7 +17,7 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin', 'win32']
+      platforms: ['darwin', 'win32'],
     },
     {
       name: '@electron-forge/maker-deb',
@@ -26,19 +27,14 @@ module.exports = {
     }
   ],
   plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-    },
-    {
-      name: '@electron-forge/plugin-fuses',
-      config: {
-        version: FuseVersion.V1,
-        fuses: {
-          [FuseV1Options.RunAsNode]: false,
-          [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-          [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-        },
-      },
-    },
-  ],
+    new AutoUnpackNativesPlugin(),
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      fuses: {
+        [FuseV1Options.RunAsNode]: false,
+        [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+        [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      }
+    })
+  ]
 };
